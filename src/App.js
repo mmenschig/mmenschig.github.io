1a94cf755b78a854c8b3464af703747afd2b371c
mmenschig <marian.menschig@googlemail.com>
2019-12-07 17:10:07 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+
+import { HashRouter, Route, Link } from 'react-router-dom';
+
+const Home = () => <h1>Home here</h1>;
+const About = () => <h1>About Content here</h1>
+
 function App() {
   return (
-    <div className="App">
-      Test content
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Navbar/>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+
+        <Route exact path="/"><Home /></Route>
+        <Route path="/about"><About /></Route>
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.css';
+
+class Navbar extends React.Component {
+
+  render() {
+    return (
+      <div className="navbar-container">
+        <div className="brand">
+          Marian <strong>Menschig</strong>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default Navbar;
